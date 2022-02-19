100640f295d727b21d101806a28dbcd4d049136b
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-02-19 09:37:36 +0200

    feat(config): more options

--- config.lua
+++ config.lua
@@ -1,5 +1,26 @@
 Config = {}
 
-Config.Coords = vector3(-1082.24, -247.55, 37.76) -- Coords for blip and box zone (qb-target)
+Config.UseQbTarget = true -- If set to false, it will use qb-menu instead
+
+Config.Coords = vector3(-1082.59, -246.88, 37.76) -- Coords for blip and box zone (qb-target) !! Changing these could cause issues.
 
 Config.TaxPercentage = 5 -- Tax Percentage
+
+Config.PayslipMenu = { -- qb-menu header
+    {
+        header = 'Check your payslip', -- Header title
+        txt = 'Check your current balance', -- Header description
+        params = {
+            event = 'cx-payslip:client:payslip', -- Don't change
+            args = 'check' -- Don't change
+        }
+    },
+    {
+        header = 'Receive Your Payslip', -- Header title
+        txt = 'Receive your current balance', -- Header description
+        params = {
+            event = 'cx-payslip:client:payslip', -- Don't change
+            args = 'receive' -- Don't change
+        }
+    }
+}
