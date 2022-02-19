f4f2ccbb00d6a0f0f0ecbb387be3f0d8a354c273
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-02-19 10:23:11 +0200

    fix(server): Add '$' signs before the balance numbers

--- server/server.lua
+++ server/server.lua
@@ -23,7 +23,7 @@ AddEventHandler('cx-payslip:server:receive', function()
             cid
         })
         player.Functions.AddMoney('cash', playerPayslip)
-        TriggerClientEvent('QBCore:Notify', src, 'You received ' .. playerPayslip)
+        TriggerClientEvent('QBCore:Notify', src, 'You received $' .. playerPayslip)
     else
         TriggerClientEvent('QBCore:Notify', src, 'Your payslip is empty', 'error')
     end
@@ -36,7 +36,7 @@ AddEventHandler("cx-payslip:server:checkPayslip", function()
     local cid = player.PlayerData.citizenid
     local playerPayslip = payslip(cid)
 
-    TriggerClientEvent('QBCore:Notify', src, "Your payslip's balance is " .. playerPayslip)
+    TriggerClientEvent('QBCore:Notify', src, "Your payslip's balance is $" .. playerPayslip)
 end)
 
 exports('AddMoney', function(cid, amount)
