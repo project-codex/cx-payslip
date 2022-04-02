669be869c93deb995ee2421a585e2f05559c1943
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-04-03 01:20:37 +0300

    feat(server): rewrite, opimize

--- server/server.lua
+++ server/server.lua
@@ -4,51 +4,42 @@ local function afterTaxes(amount)
     return amount - (amount / 100 * Config.TaxPercentage)
 end
 
-local function payslip(cid)
-    return MySQL.scalar.await('SELECT payslip FROM players WHERE citizenid = ?', {
-        cid
-    })
-end
-
-RegisterServerEvent('cx-payslip:server:receive')
-AddEventHandler('cx-payslip:server:receive', function()
-    local src = source
-    local player = QBCore.Functions.GetPlayer(src)
-    local cid = player.PlayerData.citizenid
-    local playerPayslip = payslip(cid)
+RegisterNetEvent('cx-payslip:server:receive', function()
+    local _s = source
+    local Player = QBCore.Functions.GetPlayer(_s)
+    local cid = Player.PlayerData.citizenid
+    local playerPayslip = MySQL.scalar.await('SELECT payslip FROM players WHERE citizenid = ?', { cid })
 
     if playerPayslip > 0 then
         MySQL.update("UPDATE players SET payslip=payslip-? WHERE citizenid=?;", {
             playerPayslip,
             cid
         })
-        player.Functions.AddMoney('cash', playerPayslip)
-        TriggerClientEvent('QBCore:Notify', src, 'You received $' .. playerPayslip)
+        Player.Functions.AddMoney('cash', playerPayslip)
+        TriggerClientEvent('QBCore:Notify', _s, 'You received $' .. playerPayslip)
     else
-        TriggerClientEvent('QBCore:Notify', src, 'Your payslip is empty', 'error')
+        TriggerClientEvent('QBCore:Notify', _s, 'Your payslip is empty', 'error')
     end
 end)
 
-RegisterServerEvent("cx-payslip:server:checkPayslip")
-AddEventHandler("cx-payslip:server:checkPayslip", function()
-    local src = source
-    local player = QBCore.Functions.GetPlayer(src)
-    local cid = player.PlayerData.citizenid
-    local playerPayslip = payslip(cid)
-
-    TriggerClientEvent('QBCore:Notify', src, "Your payslip's balance is $" .. playerPayslip)
+RegisterNetEvent("cx-payslip:server:checkPayslip", function()
+    local _s = source
+    local cid = QBCore.Functions.GetPlayer(_s).PlayerData.citizenid
+    local playerPayslip = MySQL.scalar.await('SELECT payslip FROM players WHERE citizenid = ?', { cid })
+    TriggerClientEvent('QBCore:Notify', _s, "Your payslip's balance is $" .. playerPayslip)
 end)
 
-exports('AddMoney', function(cid, amount, notification)
+exports('AddMoney', function(cid, amount)
     local playerPayslip = afterTaxes(amount)
-    local src = QBCore.Functions.GetPlayerByCitizenId(cid).PlayerData.source
+    local Player = QBCore.Functions.GetPlayerByCitizenId(cid)
+    local _s = Player.PlayerData.source
 
     MySQL.update("UPDATE players SET payslip=payslip+? WHERE citizenid=?;", {
         playerPayslip,
         cid,
     })
 
-    if notification and src then
-        TriggerClientEvent('QBCore:Notify', src, notification)
+    if _s then
+        TriggerClientEvent('qb-phone:client:CustomNotification', _s, 'Life Invader', '$'..amount..' were sent to your account', 'fas fa-file-invoice', '#52ff91', 5000)
     end
 end)
