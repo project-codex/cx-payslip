4bed26ab1690ae3f7573862a946c62961990b886
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-04-03 01:21:04 +0300

    feat(config): option for blip

--- config.lua
+++ config.lua
@@ -1,17 +1,22 @@
+-- To edit the blip and/or the polyzone for qb-menu, check out client/main.lua
+
 Config = {}
 
-Config.UseQbTarget = true -- If set to false, it will use qb-menu instead
+Config.UseQbTarget = false -- If set to false, it will use qb-menu instead
 
-Config.Coords = vector3(-1082.59, -246.88, 37.76) -- Coords for blip and box zone (qb-target) !! Changing these could cause issues.
+Config.Coords = vector4(-1083.95, -246.19, 37.25, 209.08) -- Coords for blip and ped
 
 Config.TaxPercentage = 5 -- Tax Percentage
+
+Config.ShowBlip = false
 
 Config.PayslipMenu = { -- qb-menu header
     {
         header = 'Check your payslip', -- Header title
         txt = 'Check your current balance', -- Header description
         params = {
-            event = 'cx-payslip:client:payslip', -- Don't change
+            isServer = true,
+            event = 'cx-payslip:server:checkPayslip', -- Don't change
             args = 'check' -- Don't change
         }
     },
@@ -19,7 +24,8 @@ Config.PayslipMenu = { -- qb-menu header
         header = 'Receive Your Payslip', -- Header title
         txt = 'Receive your current balance', -- Header description
         params = {
-            event = 'cx-payslip:client:payslip', -- Don't change
+            isServer = true,
+            event = 'cx-payslip:server:receive', -- Don't change
             args = 'receive' -- Don't change
         }
     }
